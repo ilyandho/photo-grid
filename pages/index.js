e1583d38fef7106edf8aa5f0af3b61eddbb70130
ilyandho <ii.ndhote@gmail.com>
2021-10-30 18:25:43 +0300

    added tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>PhotoGrid</title>
         <meta
@@ -14,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Photo Grid</h1>
+      <h1 className=" text-3xl">Photo Grid</h1>
     </div>
   );
 }
